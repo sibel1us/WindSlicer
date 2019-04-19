1f402aeb1da20d6f03ea15c21a4fbe7f5e32e847
Sipi Ovaska <sipi@live.fi>
2019-04-20 00:51:22 +0300

    Change window commands to run CanExecute calculation only once per HWND change

--- WindSlicer/Commands/WindowCommand.cs
+++ WindSlicer/Commands/WindowCommand.cs
@@ -14,6 +14,20 @@ namespace WindSlicer.Commands
     public abstract class WindowCommand : BaseCommand
     {
         /// <summary>
+        /// Static handle for the active window before the command window was opened.
+        /// </summary>
+        /// <remarks>
+        /// Since all window commands have identical requirements for execution, this field is used
+        /// to do the calculation once.
+        /// </remarks>
+        private static IntPtr activeHwnd = IntPtr.Zero;
+
+        /// <summary>
+        /// Whether <see cref="activeHwnd"/> is a valid handle for window commands.
+        /// </summary>
+        private static bool canExec = false;
+
+        /// <summary>
         /// Execute the window command.
         /// </summary>
         /// <param name="sender"></param>
@@ -22,8 +36,23 @@ namespace WindSlicer.Commands
 
         public sealed override bool CanExecute(object parameter)
         {
-            return
-                parameter is IntPtr hwnd &&
+            if (parameter is IntPtr hwnd)
+            {
+                if (activeHwnd != hwnd)
+                {
+                    UpdateCanExecute(hwnd);
+                }
+
+                return canExec;
+            }
+
+            return false;
+        }
+
+        private static void UpdateCanExecute(IntPtr hwnd)
+        {
+            activeHwnd = hwnd;
+            canExec =
                 hwnd != IntPtr.Zero &&
                 hwnd != NativeMethods.GetDesktopWindow() &&
                 NativeMethods.GetOpenWindowHandles().Contains(hwnd);
