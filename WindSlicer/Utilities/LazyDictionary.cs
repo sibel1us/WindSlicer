657a3d719358e735662415eb88e1abbbc8c4d496
Sipi Ovaska <sipi@live.fi>
2019-04-20 11:32:33 +0300

    Add Utils and LazyDictionary classes

---
+++ WindSlicer/Utilities/LazyDictionary.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindSlicer.Utilities
+{
+    /// <summary>
+    /// Provides a dictionary that loads dictionary values from predetermined keys using lazy
+    /// loading.
+    /// </summary>
+    /// <typeparam name="K"></typeparam>
+    /// <typeparam name="V"></typeparam>
+    public class LazyDictionary<K, V> : IReadOnlyDictionary<K, V>
+    {
+        /// <summary>
+        /// Inner dictionary containing the keys and lazy constructors.
+        /// </summary>
+        private readonly IReadOnlyDictionary<K, Lazy<V>> inner;
+
+        public V this[K key] => this.inner[key].Value;
+
+        public IEnumerable<K> Keys => this.inner.Keys;
+
+        /// <summary>
+        /// Gets an enumerable collection that contains the values of dictionary. Values are
+        /// initialized when accessed.
+        /// </summary>
+        public IEnumerable<V> Values => this.inner.Values.Select(x => x.Value);
+
+        public int Count => this.inner.Count;
+
+        /// <summary>
+        /// Initialize a new lazy loaded dictionary using the default thread safety mode <see
+        /// cref="LazyThreadSafetyMode.ExecutionAndPublication"/>.
+        /// </summary>
+        /// <param name="keys">Keys to seed the dictionary with</param>
+        /// <param name="valueFactory">
+        /// Value factory method passed into the inner <see cref="Lazy{T}"/> values.
+        /// </param>
+        public LazyDictionary(IEnumerable<K> keys, Func<K, V> valueFactory)
+            : this (keys, valueFactory, LazyThreadSafetyMode.ExecutionAndPublication)
+        { }
+
+        /// <summary>
+        /// Initialize a new lazy loaded diictionary using the specified thread safety option.
+        /// </summary>
+        /// <param name="keys">Keys to seed the dictionary with</param>
+        /// <param name="valueFactory">
+        /// Value factory method passed into the inner <see cref="Lazy{T}"/> values.
+        /// </param>
+        /// <param name="isThreadSafe">
+        /// Thread safety option passed into the inner <see cref="Lazy{T}"/> values.
+        /// </param>
+        public LazyDictionary(IEnumerable<K> keys, Func<K, V> valueFactory, bool isThreadSafe)
+            :this (keys,valueFactory,isThreadSafe
+                 ? LazyThreadSafetyMode.ExecutionAndPublication
+                 : LazyThreadSafetyMode.None)
+        { }
+
+        /// <summary>
+        /// Initialize a new lazy loaded diictionary using the specified thread safety mode. ///
+        /// </summary>
+        /// <param name="keys">Keys to seed the dictionary with</param>
+        /// <param name="valueFactory">
+        /// Value factory method passed into the inner <see cref="Lazy{T}"/> values.
+        /// </param>
+        /// <param name="mode">
+        /// Thread safety mode passed into the inner <see cref="Lazy{T}"/> values.
+        /// </param>
+        public LazyDictionary(
+            IEnumerable<K> keys,
+            Func<K, V> valueFactory,
+            LazyThreadSafetyMode mode)
+        {
+            this.inner = new ReadOnlyDictionary<K, Lazy<V>>(keys
+                .ToDictionary(x => x, x => new Lazy<V>(() => valueFactory(x), mode)));
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public bool ContainsKey(K key)
+        {
+            return this.inner.ContainsKey(key);
+        }
+
+        /// <summary>
+        /// Gets an enumerator that iterates through the dictionary and initializes the values as
+        /// needed.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator<KeyValuePair<K, V>> GetEnumerator()
+        {
+            foreach (var kvp in this.inner)
+            {
+                yield return new KeyValuePair<K, V>(kvp.Key, kvp.Value.Value);
+            }
+        }
+
+        /// <summary>
+        /// Gets the value associated with the specified key, initializing it if needed.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public bool TryGetValue(K key, out V value)
+        {
+            if (this.inner.TryGetValue(key, out Lazy<V> lazy))
+            {
+                value = lazy.Value;
+                return true;
+            }
+            else
+            {
+                value = default;
+                return false;
+            }
+        }
+
+        /// <summary>
+        /// </summary>
+        /// <returns></returns>
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
---
+++ WindSlicer/Utilities/Util.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindSlicer.Utilities
+{
+    /// <summary>
+    /// General container for helpers that aren't specific enough to warrant their own utility
+    /// classes.
+    /// </summary>
+    public static class Util
+    {
+        /// <summary>
+        /// Get possible values of <typeparamref name="T"/> using <see cref="Enum.GetValues(Type)"/>.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IEnumerable<T> ValuesOf<T>() where T : Enum
+        {
+            return Enum.GetValues(typeof(T)).Cast<T>();
+        }
+    }
+}
